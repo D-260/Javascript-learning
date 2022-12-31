bc51c91c108f85f03fc3697f279d3ed92626de35
Daksh Verma <92357759+D-260@users.noreply.github.com>
2022-12-31 12:57:06 +0530

    Update 2.3 Render.js

--- code/2.3 Render.js
+++ code/2.3 Render.js
@@ -6,6 +6,6 @@ function render() {
         todos.forEach(function (todo) {
           const element = document.createElement('div'); // creates div
           element.innerText = todo.title + ' ' + todo.dueDate; // renders(output) on screen todo item with due date
-          const todoList = document.getElementById('todo-list');  // this part of code renders output in div and not body for the ease of deletion
+          const todoList = document.getElementById('todo-list');  // this part of code renders output in div and not body for the ease of deletion as declared in line 1
           todoList.appendChild(element);
         });
