4d385692589f039e52bafb9c46f4e504f0fd3d1a
Daksh Verma <92357759+D-260@users.noreply.github.com>
2022-12-31 12:53:20 +0530

    Create 3 Delete.js

---
+++ code/3 Delete.js
@@ -0,0 +1,22 @@
+const todos = [{
+        title: 'Get groceries',
+        dueDate: '2021-10-04', // object values are separated by comma.
+        id: 'id1' // id is provided so as to identify div to be deleted.
+      },
+               
+ function addTodo() {
+        const textbox = document.getElementById('todo-title'); 
+        const title = textbox.value;
+
+        const datePicker = document.getElementById('date-picker');
+        const dueDate = datePicker.value;//already done
+
+        const id = new Date().getTime(); // gives msecs passed after 1 jan 1970 till now. used to create ids.
+
+        todos.push({
+          title: title,
+          dueDate: dueDate,
+          id: id // new property value pair
+        });
+   render();
+ }
