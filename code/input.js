5c33d45c72154e82649ce33f423e17706d0b80b6
Daksh Verma <92357759+D-260@users.noreply.github.com>
2022-12-31 11:53:16 +0530

    Create input.js

---
+++ code/input.js
@@ -0,0 +1,12 @@
+<html>
+  <head>
+    <title>My Todo App</title>
+  </head>
+  <body>
+    <input id="todo-title" type="text" /> // inputs textbox  from type = "text"
+    <button onclick="addTodo()">Add Todo</button> // trigger function on click 
+
+    <div id="todo-list"></div> // create a separate section(div) for todo list rather than body so as to manipulate elements specifically of todo list.
+  </body>
+</html>
+
